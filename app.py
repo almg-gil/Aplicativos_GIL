28b861f03fe03fc0174031ef74d2d7f4cc2bac2c
almg-gil <gpgi.gdi@gmail.com>
2026-03-17 09:13:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -735,7 +735,7 @@ class ExecutiveProcessor:
 
         self.norma_regex = re.compile(
     r'(?:^|\n|\r|\f)\s*(LEI\s+COMPLEMENTAR|LEI|DECRETO\s+NE|DECRETO)\s+N[º°]\s*([\d\s\.]+),?\s*DE\s+(.+?)(?:\n|$)',
-    re.IGNORECASE | re.DOTALL
+    re.DOTALL
         )
         self.comandos_regex = re.compile(
             r'(Ficam\s+revogados|Fica\s+acrescentado|Ficam\s+alterados|passando\s+o\s+item|passa\s+a\s+vigorar|passam\s+a\s+vigorar)',
